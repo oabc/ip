ec54e4a90a6f6ca12a1b225b49f73e48b9af9b61
myisme <you@example.com>
2017-03-06 16:09:49 +0800

    d

---
+++ cgi/getip.php
@@ -0,0 +1,17 @@
+<?php
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
+
+include '../.include.php';
+if(!get('myip'))die('127.0.0.8');
+$r=new Base\Res();
+$ip=getClientIP();
+$table='getip';
+if($db->has($table,["ip" => $ip])){
+$db->exec("UPDATE `$table` set `count`=`count`+1 where ip=$ip");  
+}
+else
+{
+$db->exec("INSERT INTO `$table` (`ip`) VALUES ('" .$ip. "')");     
+}
+$r->o($ip);
---
+++ cgi/info.php
@@ -0,0 +1,38 @@
+<?php
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
+
+require_once '../web/.main.php';
+$view['title']='GetTable IPS';
+$html=new Main(1);
+
+$e=post('exptime');
+$id=post('id');
+
+$server = $db->sel('select * from getip');
+
+?>
+<h1>Users Table</h1>
+<table class="table table-condensed">
+<tbody>
+    <tr>
+        <th>ID</th>
+        <th>ip</th>
+        <th>次数</th>
+        <th>最后时间</th>
+    </tr>
+<?php
+for($i=0;$i<count($server);$i++)
+{
+?>
+<tr>
+<td><?php echo$server[$i]['id']?></td>
+<td><?php echo$server[$i]['ip']?></td>
+<td><?php echo$server[$i]['count']?></td>
+<td><?php echo$server[$i]['time']?></td>
+</tr>
+<?php
+}
+?>
+</tbody>
+</table>
